d303dae7015824eb11760dd5047ac905b14150ab
Vadim Kravcov <v.sem4ik@mail.ru>
2023-07-11 17:34:01 +0300

    add two dir

--- src/item.py
+++ src/item.py
@@ -13,7 +13,11 @@ class Item:
         :param price: Цена за единицу товара.
         :param quantity: Количество товара в магазине.
         """
-        pass
+        self.name = name
+        self.price = price
+        self.quantity = quantity
+
+        Item.all.append(f'Item("{self.name}",{self.price},{self.quantity})')
 
     def calculate_total_price(self) -> float:
         """
@@ -21,10 +25,18 @@ class Item:
 
         :return: Общая стоимость товара.
         """
-        pass
+        return self.price*self.quantity
 
     def apply_discount(self) -> None:
         """
         Применяет установленную скидку для конкретного товара.
         """
-        pass
+        self.price = self.price * self.pay_rate
+
+
+
+# emp1 = Item('stol',12.2,10)
+# emp2 = Item('stul',34.4,45)
+# print(emp1.price)
+# emp1.apply_discount()
+# print(emp1.price)
--- tests/test_item.py
+++ tests/test_item.py
@@ -1,1 +1,11 @@
 """Здесь надо написать тесты с использованием pytest для модуля item."""
+
+import pytest
+from src.item import *
+
+@pytest.fixture
+def Item5():
+    return Item('stol',12.2,10)
+
+def test_testing(Item5):
+    assert Item5.calculate_total_price() == 122.0
